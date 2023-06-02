d76cfc6fe2e92dde005044314ebdc92da8bab9cb
Pablo Hurtado <phurtado127@gmail.com>
2023-06-01 19:37:30 -0600

    se crea la clase HelloWord2FX

--- src/main/java/com/example/helloword2/HelloApplication.java
+++ src/main/java/com/example/helloword2/HelloApplication.java
@@ -10,9 +10,12 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader root = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(root.load(), 400, 600);
+        String css=HelloApplication.class.getResource("Style/app.css").toExternalForm();
+
+        scene.getStylesheets().add(css);
+        stage.setTitle("Hola fx + css");
         stage.setScene(scene);
         stage.show();
     }
